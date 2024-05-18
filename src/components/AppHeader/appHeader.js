9cff2e555c5aba07ae4f70a35896a132414b0285
ILFT <116088209+ILFT@users.noreply.github.com>
2024-05-18 23:46:54 +0300

    0.0.3 css appheader padding

--- src/components/AppHeader/appHeader.js
+++ src/components/AppHeader/appHeader.js
@@ -9,29 +9,29 @@ function AppHeader() {
 
     return (
         <header className={styles.header}>
-            <nav className={styles.container}>
-                <ul className={styles.container}>
+            <nav className={styles.container_menu}>
+                <ul className={styles.container_menu}>
                     <li className={styles.container_menu_element}>
                         <NavLink to='/' className={({ isActive }) => (isActive ? styles.link_active : styles.link)}>
-                            <BurgerIcon className={styles.icon} type={location.pathname === '/' ? "primary" : "secondary"} />
-                            <p>Конструктор</p>
+                            <BurgerIcon type={location.pathname === '/' ? "primary" : "secondary"} />
+                            <p className={styles.icon_text}>Конструктор</p>
                         </NavLink>
                     </li>
                     <li className={styles.container_menu_element}>
                         <NavLink to='/list' className={({ isActive }) => (isActive ? styles.link_active : styles.link)}>
                             <ListIcon type={location.pathname === '/list' ? "primary" : "secondary"} />
-                            <p className="text text_type_main-default" >Лента заказов</p>
+                            <p className={styles.icon_text}>Лента заказов</p>
                         </NavLink>
                     </li>
                 </ul>
                 <NavLink className={styles.logo} to='/'>
                     <Logo />
                 </NavLink>
-                <ul className={styles.container}>
+                <ul className={styles.container_menu}>
                     <li className={styles.container_menu_element}>
                         <NavLink to='/profile' className={({ isActive }) => (isActive ? styles.link_active : styles.link)}>
                             <ProfileIcon type={location.pathname === '/profile' ? "primary" : "secondary"} />
-                            <p className="text text_type_main-default" >Личный кабинет</p>
+                            <p className={styles.icon_text} >Личный кабинет</p>
                         </NavLink>
                     </li>
                 </ul>
